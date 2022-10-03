bbcac3d853cdff2fd63eeb4c2fa0fffd97301c02
zayong <zayong@zayongui-MacBookAir.local>
2022-10-03 22:27:53 +0900

    merge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className="w-full max-w-xl mx-auto">
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
-export default MyApp
+//6.1 what is Prisma 할차례
+export default MyApp;
