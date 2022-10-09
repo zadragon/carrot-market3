211503848d34adba3ce313d5f863949ea003cc01
zayong <mase0804@naver.com>
2022-10-10 01:45:27 +0900

    user.email 등록

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div className="w-full max-w-xl mx-auto">
-      <Component {...pageProps} />
-    </div>
-  );
+	return (
+		<div className="w-full max-w-xl mx-auto">
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
-//6.6  할차례
+//6.6  할차례2
 export default MyApp;
